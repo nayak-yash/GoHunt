aab3b68bbf82186fe8afa376dcf6a85287d3d838
Yash <yashn628@gmail.com>
2024-07-11 13:10:25 +0530

    minor fixes

--- utils/cron.go
+++ utils/cron.go
@@ -10,7 +10,7 @@ import (
 func StartCronJobs() {
 	c := cron.New()
 	c.AddFunc("0 * * * *", search.RunEngine) // Run every hour
-	c.AddFunc("15 * * * *", search.RunEngine) // Run every hour at 15 mins past
+	c.AddFunc("15 * * * *", search.RunIndex) // Run every hour at 15 mins past
 	c.Start()
 	cronCount := len(c.Entries())
 	fmt.Printf("setup %d cron jobs\n", cronCount)
